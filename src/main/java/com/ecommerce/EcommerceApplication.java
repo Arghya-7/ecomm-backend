2007b3ca40655cb137d6bf4e7466119067804f37
Arghya Dey <arghyadey639@gmail.com>
2025-11-28 15:32:54 +0530

    Initial commit

---
+++ src/main/java/com/ecommerce/EcommerceApplication.java
@@ -0,0 +1,15 @@
+package com.ecommerce;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceApplication {
+	private static final Logger logger = LoggerFactory.getLogger(EcommerceApplication.class);
+	public static void main(String[] args) {
+		SpringApplication.run(EcommerceApplication.class, args);
+	}
+
+}
