c23a8fbf60b2f435dc1dcc38c74b512942394d5f
Arghya Dey <arghyadey639@gmail.com>
2025-12-18 22:38:06 +0530

    Update frontend URL in application properties and refactor image retrieval in ImageController

--- src/main/java/com/ecommerce/controllers/ImageConttroller.java
+++ src/main/java/com/ecommerce/controllers/ImageConttroller.java
@@ -1,5 +1,6 @@
 package com.ecommerce.controllers;
 
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
 import org.springframework.core.io.UrlResource;
 import org.springframework.http.HttpStatus;
@@ -13,21 +14,11 @@ import java.nio.file.Paths;
 @RequestMapping("/images")
 @CrossOrigin("*")
 public class ImageConttroller {
-    @GetMapping("/{filename}")
-    public ResponseEntity<Resource> getImage(@PathVariable String filename) {
-        try{
-            Path path = Paths.get("uploads/images/" + filename);
-            Resource resource = new UrlResource(path.toUri());
-
-            if (!resource.exists()) {
-                return ResponseEntity.notFound().build();
-            }
-
-            return ResponseEntity.ok()
-                    .contentType(MediaType.IMAGE_PNG)
-                    .body(resource);
-        } catch (Exception er){
-            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
-        }
+    @GetMapping(value = "/images/{filename}", produces = MediaType.IMAGE_PNG_VALUE)
+    public ResponseEntity<Resource> getImages(@PathVariable String filename) {
+        Resource resource = new ClassPathResource("static/images/" + filename);
+        return ResponseEntity.ok()
+                .contentType(MediaType.IMAGE_PNG)
+                .body(resource);
     }
 }
