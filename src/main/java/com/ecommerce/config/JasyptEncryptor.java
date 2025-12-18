288d398fc3fccf07b7772112ac0990c4749bea54
Arghya Dey <arghyadey639@gmail.com>
2025-12-18 18:27:24 +0530

    Update MongoDB URI in application properties and change frontend URL

--- src/main/java/com/ecommerce/config/JasyptEncryptor.java
+++ src/main/java/com/ecommerce/config/JasyptEncryptor.java
@@ -8,11 +8,7 @@ public class JasyptEncryptor {
     public static void main(String[] args) {
         AES256TextEncryptor encryptor = new AES256TextEncryptor();
         encryptor.setPassword("Arghyadey@789"); // your master key
-        String encrypted = encryptor.encrypt("5367566859703373367639792F423F452848284D6251655468576D5A71347437");
+        String encrypted = encryptor.encrypt("mongodb://mongo:UMfJzWIEMoeDBsnHggkTQUGzAZeurJSD@metro.proxy.rlwy.net:14410/ecommerce?authSource=admin");
         System.out.println(encrypted);
-
-        LocalDateTime now = LocalDateTime.now();
-        LocalDateTime afterAddingFiveMints = LocalDateTime.now().plusMinutes(2);
-        System.out.println(now.isBefore(afterAddingFiveMints));
     }
 }
